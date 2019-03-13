85463103cc6b3a582b9e9a2b0e10bd27d4cc9756
LittleGuava <f.nunes05@outlook.com>
2019-03-13 13:48:15 +0000

    Chamando as gems devise e factory_girl

--- lib/api_version_constraint.rb
+++ lib/api_version_constraint.rb
@@ -2,7 +2,7 @@ class ApiVersionConstraint
     #recebe o parametro options, pega a versão e guarda
     def initialize(options)
         @version = options[:version]
-        @ddefault = options[:default]
+        @default = options[:default]
     end
     # pega a requisição, vai verificar no cabeçalho acccept se tem o conteúdo do include (futuramente descobriremos o que é)
     # os defaults era para verificar a versão defaults
