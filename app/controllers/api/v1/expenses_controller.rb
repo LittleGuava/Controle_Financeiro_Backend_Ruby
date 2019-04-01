4492154d7c282a1e7e3c3c176a12cd0addda915d
andre-tlima <andre-tlima@hotmail.com>
2019-04-01 18:28:37 +0000

    Correção de bugs

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ExpensesController < ApplicationController
     before_action :authenticate_with_token!
     
     def index
-<<<<<<< HEAD
+
         expenses = current_user.expenses
         render json: {expenses: expenses}, status: 200
     end
@@ -19,7 +19,7 @@ class Api::V1::ExpensesController < ApplicationController
             render json: expense, status: 201
         else
             render json: {errors: expense.errors}, status: 422
-=======
+
         gains = current_user.gains
         render json: {gains: gains}, status: 200
     end
@@ -35,46 +35,46 @@ class Api::V1::ExpensesController < ApplicationController
             render json: gain, status: 201
         else
             render json: {errors: gain.errors}, status: 422
->>>>>>> adding-rec-des
+
         end
     end
     
     def update
-<<<<<<< HEAD
+
         expense = current_user.expenses.find(params[:id])
         if expense.update_attributes(expense_params)
             render json: expense, status: 200
         else
             render json: {errors: expense errors}, status: 422
-=======
+
         gain = current_user.gains.find(params[:id])
         if gain.update_attributes(gain_params)
             render json: gain, status: 200
         else
             render json: {errors: gain errors}, status: 422
->>>>>>> adding-rec-des
+
         end
     end
     
     def destroy
-<<<<<<< HEAD
+
         expense = current_user.expenses.find(params[:id])
         expense.destroy
-=======
+
         gain = current_user.gains.find(params[:id])
         gain.destroy
->>>>>>> adding-rec-des
+
         head 204
     
     private
     
-<<<<<<< HEAD
+
     def expense_params
         params.require(:expense).permit(:description, :value, :date)
-=======
+
     def gain_params
         params.require(:gain).permit(:description, :value, :date)
->>>>>>> adding-rec-des
+
     end
     
 end
